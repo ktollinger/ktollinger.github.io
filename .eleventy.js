29740f3713be2c4fcfc56f249929222b1ab00a05
Kamil Tollinger <kamil.tollinger@gmail.com>
2024-09-21 06:00:57 +0200

    custom layout

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,12 @@
 module.exports = function(eleventyConfig) {
     return {
       dir: {
-        input: "src", // Where your content lives
-        output: "_site" // Where Eleventy will generate your website
+        // Where your content lives
+        input: "src",
+        // layouts directory - relative to the dir.inpur
+        layouts: "_layouts",
+        // Where Eleventy will generate your website
+        output: "_site",
       }
     };
   };
